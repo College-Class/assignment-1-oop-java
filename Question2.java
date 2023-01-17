0661fbc884174ece7a32c6057fdaa2f35078bfec
Rabin Acharya <charms2074@gmail.com>
2023-01-17 08:47:39 +0545

    question 2 formated

--- Question2.java
+++ Question2.java
@@ -1,31 +1,25 @@
 public class Question2 {
-    
 
-    public static void main(String[] args) { 
-double eggsOnMonday = 100; //given
-double eggsOnTuesday = 121 ; //given
-double eggsOnWednesday  =  117 ;//given  
-double totalEggs = eggsOnMonday+eggsOnTuesday+eggsOnWednesday;  //adding all the eggs collected per day
-double dailyAvg = totalEggs /3 ; // 3: being number of days 
-double monthlyAvg = dailyAvg * 30 ;  // calculating monthly average of eggs that can be collected
+    public static void main(String[] args) {
+        double eggsOnMonday = 100;  
+        double eggsOnTuesday = 121;  
+        double eggsOnWednesday = 117; 
+        double totalEggs = eggsOnMonday + eggsOnTuesday + eggsOnWednesday;              // adding all the eggs collected per day
+        double dailyAvg = totalEggs / 3;                // 3: being number of days
+        double monthlyAvg = dailyAvg * 30;          // calculating monthly average of eggs that can be collected
 
-double profitPerEgg = 0.18;  // profit per egg
+        double profitPerEgg = 0.18; // profit per egg
 
-// Printing the output now ;  
+        // Printing the output now ;
 
+        // Daily Avg :
+        System.out.println("Daily Average : " + dailyAvg);
 
-// Daily Avg : 
-System.out.println("Daily Average : " + dailyAvg); 
+        // Monthly Avg :
+        System.out.println("Monthly Average : " + monthlyAvg);
 
-//Monthly Avg : 
-System.out.println("Monthly Average : " + monthlyAvg); 
-
-// Monthly Profit from eggs 
-System.out.println("Profit: $" + profitPerEgg * monthlyAvg ); 
-
-
-
-
+        // Monthly Profit from eggs
+        System.out.println("Profit: $" + profitPerEgg * monthlyAvg);
 
     }
 }
